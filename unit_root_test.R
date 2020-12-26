193075118c5963f78d1401c753cdc2646c177c40
CristhelC <anacristhel.cl@gmail.com>
2020-12-26 10:24:26 -0500

    Unit Root Test

---
+++ unit_root_test.R
@@ -0,0 +1,21 @@
+install.packages("xelatex")
+install.packages(c("ggplot2",'RColorBrewer','tinytex'))
+remotes::install_github("crsh/papaja", dependencies = TRUE)
+install.packages('remotes')
+install.packages('tinytex')
+library(urca)
+Paridad<- read.csv(
+  file = 'Datos_Paridad.csv',
+  stringsAsFactors = FALSE, 
+  strip.white = TRUE,
+  sep = ';'
+)
+head(Paridad)
+View(Paridad)
+summary(Paridad)
+attach(Paridad)
+hist(Inflacion_China,main = '',xlab = 'Rangos',
+     ylab = 'Frecuencia',col = '#336BFF',breaks = 30,xlim = c(-0.02,0.1))
+?hist
+ur.ers(Inflacion_China)
+?`urca-class`
