49363998d35a37cf0bf37c4e5c32b2fa3b5aa8c7
CristhelC <anacristhel.cl@gmail.com>
2020-12-27 15:16:56 -0500

    Pruebas estadísticas

--- unit_root_test.R
+++ unit_root_test.R
@@ -15,8 +15,34 @@ View(Paridad)
 summary(Paridad)
 attach(Paridad)
 hist(Inflacion_China,main = '',xlab = 'Rangos',
-     ylab = 'Frecuencia',col = '#336BFF',breaks = 30,xlim = c(-0.02,0.1))
+     ylab = 'Frecuencia',col = '#3361FF',breaks = 20,xlim = c(-0.02,0.1),
+     ylim=c(0.0,50))
 ?hist
 ur.ers(Inflacion_China)
 ?`urca-class`
 hfghuhgjdksgndn
+writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+Sys.which("make")
+## "C:\\rtools40\\usr\\bin\\make.exe"
+install.packages("jsonlite", type = "source")
+if(!"devtools" %in% rownames(installed.packages())) install.packages("devtools")
+devtools::install_github("crsh/papaja")
+#plot
+plot(Tipo_cambio,type = 'l',xlab = 'Rangos',ylab = 'Valores',col='#3361FF')
+plot(Inflacion_China,type = 'l',xlab = 'Rangos',ylab = 'Valores',col='#3361FF')
+plot(Inflacion_Peru,type = 'l',xlab = 'Rangos',
+     ylab = 'Valores',col='#3361FF',ylim = c(0.00,0.08),xlim = c(0.0,200))
+#histogramas
+hist(Inflacion_Peru,main = '',xlab = 'Rangos',
+     ylab = 'Frecuencia',col = '#3361FF',breaks = 20,xlim = c(-0.02,0.08),
+     ylim = c(0.0,40))
+ur.ers(Tipo_cambio)
+ur.ers(Inflacion_Peru)
+ur.ers(Inflacion_China)
+ers.tipocambio<-ur.ers(Tipo_cambio,type ='DF-GLS',model = 'trend')
+summary(ers.tipocambio)
+ur.kpss(Tipo_cambio)
+ur.pp(Tipo_cambio)
+ers.tipocambio<-ur.ers(Tipo_cambio,type ='DF-GLS',model = 'trend',
+                       lag.max = 1)
+summary(ers.tipocambio)
